ee4af5a80e6b1e4303031621136a28f756094620
Martina Ponce de Leon <MARTIPONCEDELEON03@ies21.onmicrosoft.com>
2024-04-03 22:56:02 -0300

    frmFirma Terminado

--- frmFirma.Designer.cs
+++ frmFirma.Designer.cs
@@ -28,50 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.picFirma = new System.Windows.Forms.PictureBox();
-            this.btnGuardar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picFirma)).BeginInit();
-            this.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFirma));
+            picFirma = new PictureBox();
+            btnGuardar = new Button();
+            ((System.ComponentModel.ISupportInitialize)picFirma).BeginInit();
+            SuspendLayout();
             // 
             // picFirma
             // 
-            this.picFirma.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.picFirma.Location = new System.Drawing.Point(12, 12);
-            this.picFirma.Name = "picFirma";
-            this.picFirma.Size = new System.Drawing.Size(610, 558);
-            this.picFirma.TabIndex = 0;
-            this.picFirma.TabStop = false;
-            this.picFirma.Paint += new System.Windows.Forms.PaintEventHandler(this.picFirma_Paint);
-            this.picFirma.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picFirma_MouseMove);
+            picFirma.BackColor = Color.LavenderBlush;
+            picFirma.Location = new Point(12, 12);
+            picFirma.Name = "picFirma";
+            picFirma.Size = new Size(610, 558);
+            picFirma.TabIndex = 0;
+            picFirma.TabStop = false;
+ //           picFirma.Paint += picFirma_Paint;
+            picFirma.MouseMove += picFirma_MouseMove;
             // 
             // btnGuardar
             // 
-            this.btnGuardar.BackColor = System.Drawing.Color.Plum;
-            this.btnGuardar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.btnGuardar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnGuardar.Location = new System.Drawing.Point(547, 576);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
-            this.btnGuardar.TabIndex = 1;
-            this.btnGuardar.Text = "Guardar";
-            this.btnGuardar.UseVisualStyleBackColor = false;
-            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            btnGuardar.BackColor = Color.LavenderBlush;
+            btnGuardar.Cursor = Cursors.Hand;
+            btnGuardar.FlatAppearance.BorderColor = Color.FromArgb(192, 0, 192);
+            btnGuardar.FlatAppearance.BorderSize = 15;
+            btnGuardar.FlatStyle = FlatStyle.Popup;
+            btnGuardar.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnGuardar.Location = new Point(541, 576);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(81, 23);
+            btnGuardar.TabIndex = 1;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = false;
+            btnGuardar.Click += btnGuardar_Click;
             // 
             // frmFirma
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 611);
-            this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.picFirma);
-            this.Name = "frmFirma";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dibujar Firma";
-            this.Load += new System.EventHandler(this.frmFirma_Load);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmFirma_MouseMove);
-            ((System.ComponentModel.ISupportInitialize)(this.picFirma)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(634, 611);
+            Controls.Add(btnGuardar);
+            Controls.Add(picFirma);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmFirma";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Dibujar Firma";
+ //           Load += frmFirma_Load;
+//            MouseMove += frmFirma_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)picFirma).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
